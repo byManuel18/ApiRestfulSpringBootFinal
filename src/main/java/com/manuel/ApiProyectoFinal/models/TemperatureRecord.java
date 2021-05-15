ba6904a269c64513a4254bfc5c196007b770c2d3
Manuel <antoniomanu533@gmail.com>
2021-05-15 17:25:39 +0200

    make it better

--- src/main/java/com/manuel/ApiProyectoFinal/models/TemperatureRecord.java
+++ src/main/java/com/manuel/ApiProyectoFinal/models/TemperatureRecord.java
@@ -32,7 +32,7 @@ public class TemperatureRecord {
 	private Date date;
 	
 	@NotBlank
-	@ManyToOne(cascade=CascadeType.MERGE,fetch=FetchType.EAGER)
+	@ManyToOne(cascade= {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.EAGER)
 	@JoinColumn(name="id_appliance")
 	private Appliance appliance;
 	
